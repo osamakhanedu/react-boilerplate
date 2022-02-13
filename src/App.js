d157f1b1ca75b9ca2e3a365a9166529f3a148a8b
Osama Khan <osama.khan@tabsera.com>
2022-02-13 18:46:58 +0500

    added css-loader and style-loader; so it work with webpack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "./style.css";
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function App() {
-  return <h1>Hell World!!!</h1>;
+  render() {
+    return (
+      <div>
+        <h3>{this.state.count}</h3>
+        <button
+          onClick={() => {
+            this.setState((prevState) => ({ count: prevState.count + 1 }));
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            this.setState((prevState) => ({ count: prevState.count - 1 }));
+          }}
+        >
+          -
+        </button>
+        <h1>Hell World!!!</h1>;
+      </div>
+    );
+  }
 }
 
 export default App;
